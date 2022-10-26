bc61d9d3b5dca6175a72f5de1de55156b3fb5cc1
rjordell <rjordell@gmail.com>
2022-10-25 19:43:52 -0700

    Created unit tests for Hailstone.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,26 @@
 #include "gtest/gtest.h"
-#include "../include/Triangle.h"
-using shapes::Triangle;
+#include "../include/Hailstone.h"
+using sequence::satisfiesHailstone;
 
 
-TEST(TriangleTests, testPerimeter) {
+/*TEST(HailstoneTests, testZero) {
     Triangle *aTriangle = new Triangle(3,3,3);
-    EXPECT_EQ (aTriangle->getPerimeter(),9);
+    EXPECT_FALSE (aTriangle->getPerimeter(),false);
+}
+*/
+
+TEST(HailstoneTests, testZero) {
+    EXPECT_FALSE (satisfiesHailstone(0));
+}
+
+TEST(HailstoneTests, testOne) {
+    EXPECT_TRUE (satisfiesHailstone(1));
+}
+
+TEST(HailstoneTests, testFour) {
+    EXPECT_TRUE (satisfiesHailstone(4));
+}
+
+TEST(HailstoneTests, testThree) {
+    EXPECT_TRUE (satisfiesHailstone(3));
 }
