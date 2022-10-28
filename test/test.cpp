1dd0c9bf5fedf90a0e85e6ed2f0d1a614c9c50fb
itskristnn <itskristnn@gmail.com>
2022-10-28 03:08:09 -0700

    Modified test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -4,23 +4,23 @@ using sequence::satisfiesHailstone;
 
 
 /*TEST(HailstoneTests, testZero) {
- *     Triangle *aTriangle = new Triangle(3,3,3);
- *         EXPECT_FALSE (aTriangle->getPerimeter(),false);
- *         }
- *         */
+ * *     Triangle *aTriangle = new Triangle(3,3,3);
+ * *         EXPECT_FALSE (aTriangle->getPerimeter(),false);
+ * *         }
+ * *         */
 
 TEST(HailstoneTests, testZero) {
-	    EXPECT_FALSE (satisfiesHailstone(0));
+    EXPECT_FALSE (satisfiesHailstone(0));
 }
 
 TEST(HailstoneTests, testOne) {
-	    EXPECT_TRUE (satisfiesHailstone(1));
+    EXPECT_TRUE (satisfiesHailstone(1));
 }
 
 TEST(HailstoneTests, testFour) {
-	    EXPECT_TRUE (satisfiesHailstone(4));
+    EXPECT_TRUE (satisfiesHailstone(4));
 }
 
 TEST(HailstoneTests, testThree) {
-	    EXPECT_TRUE (satisfiesHailstone(3));
+    EXPECT_TRUE (satisfiesHailstone(3));
 }
