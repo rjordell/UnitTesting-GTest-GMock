b018b8244287a8fcbba75b57fb0896c0f2341a34
itskristnn <itskristnn@gmail.com>
2022-10-28 15:55:01 -0700

    Modified CMakeLists.txt and test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,33 @@
 #include "gtest/gtest.h"
-#include "../include/Triangle.h"
-using shapes::Triangle;
+#include "gmock/gmock.h"
+#include "../include/Awards.h"
+using awards::RankList;
+using awards::AwardCeremonyActions;
 
+class MockAwardCeremonyActions : public AwardCeremonyActions { 
+	public: 
+		MOCK_METHOD(void, playAnthem , (), (override)); 
+		MOCK_METHOD(void, awardBronze, (std::string recepient), (override)); 
+		MOCK_METHOD(void, awardSilver, (std::string recepient), (override)); 
+		MOCK_METHOD(void, awardGold, (std::string recepient), (override)); 
+		MOCK_METHOD(void, turnOffLightsAndGoHome, (), (override)); 
+};
 
-TEST(TriangleTests, testPerimeter) {
-    Triangle *aTriangle = new Triangle(3,3,3);
-    EXPECT_EQ (aTriangle->getPerimeter(),9);
+class StubRankList : public RankList {
+	public:
+		int counter = 0;
+		std::string list[3];
+		StubRankList() {
+			list[0] = "a";
+			list[1] = "b";
+			list[2] = "c";
+		}
+		virtual std::string getNext() {return list[counter++];}
+};
+
+TEST(AwardsTest, testAwardsCeremony) {
+	MockAwardsCeremonyActions mockAwardsCeremony;
+	StubRankList stubList;
+	EXPECT_CALL(mockAwardsCeremony, awardBronze("a"));
+	performAwardCeremony(stubList, mockAWardsCeremony);
 }
